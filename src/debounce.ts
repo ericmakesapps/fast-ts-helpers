a81cb8430bf30a708aa901bf45f6378c74ce13a3
Eric Ferreira <2483476+ericbf@users.noreply.github.com>
2022-08-11 21:09:36 -0400

    Allow debounce to pass return value if immediate is true
    This will allow you to use debounce for API type calls and other stuff that should return a value.

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,17 +1,30 @@
 /**
  * Debounce the passed function. This means that the function will only fire once while it is being called within the threshold.
  *
- * @param func The function to be debounced.
+ * @param func The function to be debounced. The return value will only be passed through if immediate is true.
  * @param threshold How long to wait before allowing the function to be triggered again. Default is `100`.
  * @param immediate Whether to trigger the function at the start instead of after the threshold. Default is `false`.
  * @returns A version of this function that will only trigger when it isn't called for the threshold amount of time.
  */
 export function debounce<T extends (...args: any[]) => void>(
 	func: T,
+	threshold: number,
+	immediate: false
+): T
+export function debounce<T extends (...args: any[]) => unknown>(
+	func: T,
+	threshold: number,
+	immediate: boolean
+): T
+
+export function debounce<T extends (...args: any[]) => unknown>(
+	func: T,
 	threshold = 100,
 	immediate = false
 ) {
 	let timeout: number | undefined
+
+	let returnVal: unknown
 
 	return function (this: T, ...args: unknown[]) {
 		const call = () => {
@@ -23,10 +36,16 @@ export function debounce<T extends (...args: any[]) => void>(
 		}
 
 		if (immediate && timeout == null) {
-			func.apply(this, args)
+			returnVal = func.apply(this, args)
 		}
 
 		clearTimeout(timeout)
 		timeout = setTimeout(call, threshold)
+
+		if (immediate) {
+			return returnVal
+		}
+
+		return undefined
 	} as T
 }
