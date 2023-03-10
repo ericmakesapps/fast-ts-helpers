86fce7ecf686412c302988c6e2ae1446655c9cc9
Eric Ferreira <eric@ericmakesapps.com>
2023-03-10 17:52:25 -0500

    [2.1.0] Add `updatingState` and `updatingStateRef` helpers
    These helpers allow you to use a `useState` (or `useStateRef`) in conjunction with a `useEffect` to update it as some external value changes.

---
+++ src/useUpdatingState.ts
@@ -0,0 +1,38 @@
+import { DependencyList, Dispatch, SetStateAction, useEffect, useState } from "react"
+
+import isCallable from "./isCallable"
+
+/**
+ * This state variable will update to the value passed to it if it changed. It's useful
+ * for when the state value also needs to track an external value.
+ */
+function useUpdatingState<T>(
+	value: T & Exclude<T, Function>
+): [T, Dispatch<SetStateAction<T>>]
+function useUpdatingState<T>(
+	getter: (currentValue?: T) => T,
+	deps: DependencyList
+): [T, Dispatch<SetStateAction<T>>]
+
+function useUpdatingState<T>(
+	valueOrGetter: (T & Exclude<T, Function>) | ((currentValue?: T) => T),
+	deps?: DependencyList
+) {
+	const state = useState(valueOrGetter)
+
+	useEffect(
+		() => {
+			state[1](valueOrGetter)
+		},
+		// Since `deps` is the dependency list for `valueOrGetter` if it is callable, we
+		//   pass it directly as the list in that case. However if `valueOrGetter` is not
+		//   callable, we use its value as the dependency in the dependency list.
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		isCallable(valueOrGetter) ? deps : [valueOrGetter]
+	)
+
+	return state
+}
+
+export default useUpdatingState
---
+++ src/useUpdatingStateRef.ts
@@ -0,0 +1,44 @@
+import {
+	DependencyList,
+	Dispatch,
+	MutableRefObject,
+	SetStateAction,
+	useEffect
+} from "react"
+
+import isCallable from "./isCallable"
+import useStateRef from "./useStateRef"
+
+/**
+ * This state/ref variable will update to the value passed to it if it changed, while also
+ * having access to the value through a react ref. It's useful for when the state/ref
+ * value also needs to track an external value.
+ */
+export function useUpdatingStateRef<T>(
+	value: T & Exclude<T, Function>
+): [Readonly<MutableRefObject<T>>, Dispatch<SetStateAction<T>>, T]
+export function useUpdatingStateRef<T>(
+	getter: (currentValue?: T) => T,
+	deps: DependencyList
+): [Readonly<MutableRefObject<T>>, Dispatch<SetStateAction<T>>, T]
+
+export function useUpdatingStateRef<T>(
+	valueOrGetter: (T & Exclude<T, Function>) | ((currentValue?: T) => T),
+	deps?: DependencyList
+) {
+	const state = useStateRef(valueOrGetter)
+
+	useEffect(
+		() => {
+			state[1](valueOrGetter)
+		},
+		// Since `deps` is the dependency list for `valueOrGetter` if it is callable, we
+		//   pass it directly as the list in that case. However if `valueOrGetter` is not
+		//   callable, we use its value as the dependency in the dependency list.
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		isCallable(valueOrGetter) ? deps : [valueOrGetter]
+	)
+
+	return state
+}
