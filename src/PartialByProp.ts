87df6c41d0d530c25054a9a81098ade4b17a51b4
Eric Ferreira <eric@ericmakesapps.com>
2023-05-20 19:52:55 -0400

    Adjust docs for `PartialByProp`, remove duplicate
    The docs for `PartialByProp` were a little unclear. This commit fixes that. We also had a duplicate helper for that same functionality, `PartialProps`, which sounds like it should get the props that are optional, but it actually just make some props partial.

--- src/PartialByProp.ts
+++ src/PartialByProp.ts
@@ -1,10 +1,11 @@
 /**
- * Make only specific properties of a type optional.
+ * Make specific properties of a type optional and potentially undefined.
  *
- * @template Type The type in which to make some properties optional.
- * @template Key The properties of the type to make optional.
+ * @template Type The type in which to make some properties optional and potentially undefined.
+ * @template Key The properties of the type to make optional and potentially undefined.
  */
-type PartialByProp<Type extends object, Key extends keyof Type> = Omit<Type, Key> &
-	Partial<Pick<Type, Key>>
+type PartialByProp<Type extends object, Key extends keyof Type> = Omit<Type, Key> & {
+	[P in Key]?: Type[P] | undefined
+}
 
 export default PartialByProp
--- src/PartialProps.ts
+++
@@ -1,6 +0,0 @@
-/** Make a type partial only for certain props. */
-type PartialProps<T, K extends keyof T> = Omit<T, K> & {
-	[P in K]?: T[P]
-}
-
-export default PartialProps
