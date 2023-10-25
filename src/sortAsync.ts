4f7eefb9373ab12e761b1f86d3f4689fbd6e318d
Eric Ferreira <eric@ericmakesapps.com>
2023-10-25 17:41:47 -0400

    [4.1.1] Use locale sort for strings in sort async
    The default JS sort is not so good, so this uses the more useful `localeCompare` for strings.

--- src/sortAsync.ts
+++ src/sortAsync.ts
@@ -5,13 +5,19 @@ import tuple from "./tuple"
  *   ascending.
  *
  * @param compareValue The value to sort on. If you need access to the original doc, return a tuple or something from here.
- * @param compareFn The sort function. Defaults to an ascending sort based on `>` and `<`.
+ * @param compareFn The sort function. Defaults to a locale compare using US locale for strings, and otherwise an ascending sort based on `>` and `<`.
  * @returns A promise that resolves to the sorted array.
  */
 function sortAsync<T, U>(
 	compareValue: (value: T) => U,
 	compareFn: (a: Awaited<U>, b: Awaited<U>) => number = (l, r) =>
-		l > r ? 1 : l < r ? -1 : 0
+		typeof l === "string" && typeof r === "string"
+			? l.localeCompare(r, "en-US", { numeric: true, sensitivity: "base" })
+			: l > r
+			? 1
+			: l < r
+			? -1
+			: 0
 ): (array: T[]) => Promise<T[]> {
 	return async (array: T[]) => {
 		const withSortValues = await Promise.all(
