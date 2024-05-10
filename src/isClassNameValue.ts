b02aa47a066026418bedab17906a33a9c258d103
Eric Ferreira <eric@ericmakesapps.com>
2024-05-10 19:06:34 -0400

    [5.1.0] Import `ClassNameValue` from peer dependency `tailwind-merge`
    We want to use the appropriate version for your `tailwind-merge` version. This uses the peer dependency instead.

--- src/isClassNameValue.ts
+++ src/isClassNameValue.ts
@@ -1,14 +1,11 @@
-type ClassNameValue = ClassNameArray | string | null | undefined | 0 | false
-type ClassNameArray = ClassNameValue[]
+import { ClassNameValue } from "tailwind-merge"
 
 /**
  * Check whether the passed param is a ClassNameValue. Tailwind's tree shaking requires that you put classes in `className` or `class` props, so passing non-standard class name props breaks it. The work-around is to pass additional classes through objects in the `className` prop.
  * @param param The object or ClassNameValue to check
  * @returns Whether the passed param is a ClassNameValue.
  */
-function isClassNameValue(
-	param: Record<string, any> | ClassNameValue
-): param is ClassNameValue {
+function isClassNameValue(param: ClassNameValue): param is ClassNameValue {
 	return !(typeof param === "object" && !Array.isArray(param))
 }
 
