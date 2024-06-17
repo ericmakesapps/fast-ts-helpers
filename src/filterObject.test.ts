90061f968c7ebf9759b1fbae1bcc933d1b0c7ad2
Eric Ferreira <eric@ericmakesapps.com>
2024-06-17 18:16:56 -0400

    [5.3.0] Add `filterObject` helper
    This helper will filter all falsey values from an object, removing those keys from the object. This guarantees that the object will only have truthy values in it, or, rather, it will only have keys whose values or truthy. The resulting type will have all definitely truthy values as defined, all maybe falsey values as optional, and all definitely falsey values as not present.

---
+++ src/filterObject.test.ts
@@ -0,0 +1,52 @@
+import filterObject from "./filterObject"
+
+describe("filterObject helper", () => {
+	it("should return a copy of the passed object with all the falsey values filtered out", () => {
+		const testObject = {
+			foo: "bar",
+			bar: 0,
+			baz: false,
+			qux: null,
+			quux: undefined,
+			corge: NaN,
+			grault: ""
+		}
+
+		const result = filterObject(testObject)
+
+		expect(result).not.toBe(testObject)
+		expect(result).toEqual({
+			foo: "bar"
+		})
+	})
+
+	it("should return an empty object if all values are falsey", () => {
+		const testObject = {
+			foo: 0,
+			bar: false,
+			baz: null,
+			qux: undefined,
+			quux: NaN,
+			corge: ""
+		}
+
+		const result = filterObject(testObject)
+
+		expect(result).toEqual({})
+	})
+
+	it("should return a copy of the same object if all values are truthy", () => {
+		const testObject = {
+			foo: "bar",
+			bar: 42,
+			baz: true,
+			qux: {},
+			quux: [],
+			corge: new Date()
+		}
+
+		const result = filterObject(testObject)
+
+		expect(result).toEqual(testObject)
+	})
+})
---
+++ src/filterObject.ts
@@ -0,0 +1,62 @@
+import Falsey from "./Falsey"
+import IdType from "./IdType"
+import NonFalsible from "./NonFalsible"
+
+// Utility type to filter properties
+type DefinitelyTruthyProps<T> = Exclude<
+	{
+		[K in keyof T]: Extract<T[K], string | number | boolean | Falsey> extends never
+			? K
+			: never
+	}[keyof T],
+	undefined
+>
+
+type DefinitelyFalseyProps<T> = Exclude<
+	{
+		[K in keyof T]: T[K] extends Falsey ? K : never
+	}[keyof T],
+	undefined
+>
+
+// Utility type to make properties optional if they could be falsey
+type MaybeFalseyProps<T> = Exclude<
+	{
+		[K in keyof T]: T[K] extends string | number | boolean | object | null | undefined
+			? K
+			: never
+	}[keyof T],
+	DefinitelyFalseyProps<T> | undefined
+>
+
+// Utility type to exclude falsey properties and make optional properties that could be falsey
+type FilteredObject<T> = IdType<
+	{
+		[K in DefinitelyTruthyProps<T>]: T[K]
+	} & {
+		[K in MaybeFalseyProps<T>]?: NonFalsible<T[K]>
+	}
+>
+
+/**
+ * Filter out all falsey values from the passed object. This will filter out `0`s, `NaN`s,
+ *   `""`s, `false`s, `null`s, and `undefined`s. Don't use this if you need to retain any
+ *   of these values.
+ *
+ * @template Type The type of the elements in the array.
+ * @param object The object to filter.
+ * @returns A copy of the passed object with all falsey properties filtered out.
+ */
+function filterObject<Type extends object>(object: Type): FilteredObject<Type> {
+	const clonedObject: any = {}
+
+	for (const key in object) {
+		if (object[key]) {
+			clonedObject[key] = object[key]
+		}
+	}
+
+	return clonedObject
+}
+
+export default filterObject
