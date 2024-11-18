19450278dca041d1c6c9cf934442e1dd6502fb00
Eric Ferreira <eric@ericmakesapps.com>
2024-11-18 11:54:04 -0500

    [6.6.1] Fix number pattern in `isJsonlike`
    The pattern should match any valid number. This commit fixes the pattern accordingly, now matching leading zeros, as well as octal or scientific notation.

--- src/isJsonlike.ts
+++ src/isJsonlike.ts
@@ -1,6 +1,6 @@
 /** Test whether a given string is JSON-like. This doesn't do any real validation. */
 export default function isJsonlike(value: string) {
-	return /^(\s\n)*(-?[1-9][0-9]*(\.[0-9]+)?|null|".*"|\{(.|\n)*\}|\[(.|\n)*\])(\s\n)*$/.test(
+	return /^(\s\n)*(-?[0-9](_?[0-9])*(\.[0-9](_?[0-9])*)?|0x[0-9](_?[0-9])*|[0-9](_?[0-9])*e[0-9](_?[0-9])*|null|".*"|\{(.|\n)*\}|\[(.|\n)*\])(\s\n)*$/i.test(
 		value
 	)
 }
