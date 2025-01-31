da8a44db0b40bf0f48a4b722d348c35a26ef0fd5
Eric Ferreira <eric@ericmakesapps.com>
2025-01-31 10:57:30 -0500

    [6.8.3] Fix nullable entries in `entries`
    Entries will never return undefined as an entry tuple. This fixes that type issue.

--- src/entries.ts
+++ src/entries.ts
@@ -2,5 +2,7 @@ import Stringlike from "./Stringlike"
 
 /** Get a properly typed array of the entries of the passed object. */
 export default function entries<T extends object>(obj: T) {
-	return Object.entries(obj) as { [K in keyof T]: [Stringlike<K>, T[K]] }[keyof T][]
+	return Object.entries(obj) as NonNullable<
+		{ [K in keyof T]: [Stringlike<K>, T[K]] }[keyof T]
+	>[]
 }
